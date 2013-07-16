504255f377bd25c59ea35641e34a84bd884ddd88
beckastar <rebecca.robbins.et@gmail.com>
2013-07-16 15:37:26 -0700

    Pushing to git

--- csvtest.py
+++ csvtest.py
@@ -9,19 +9,32 @@ with open('seed_data/u.data', 'rb') as csvfile:
 		print 'this is item_id ->', row[1]
 		print 'this is rating ->', row[2]
 		print 'this is timestam ->', row[3]
-
-#i removed the .join function from the above loop for debugging purposes
-#and because i think we want to information in discrete chunks
-		print ', '.join(row)
-
+# #i removed the .join function from the above loop for debugging purposes
+# #and because i think we want to information in discrete chunks
+# 		print ', '.join(row)
 
 
+
+
+# from model import Rating 
+# import csv
 
 # with open('seed_data/u.data', 'rb') as csvfile:
 # 	f = csv.reader(csvfile, delimiter = '	', quotechar='|')
-# 	counter = 10
-# 	while counter >0:
-# 		for row in f:
-# 			print ',  '.join(row)
-# 		counter = counter - 1
+# 	for row in f:
+# 		rating = Ratings(movie_id=row[1], user_id=row[0], rating=row[2])
+# 		session.add(rating)
 
+# # 		session.commit()
+
+
+# with open('seed_data/u.user', 'rb') as csvfile:
+#     f = csv.reader(csvfile, delimiter = "  ", quotechar='|')
+#     for row in f:
+#     	print row 
+#     	# print row[1][0]
+#     	# print row[3]
+#     	# print row[4]
+        #     user = User(age=row[1], zipcode=row[4])
+        #     session.add(user)
+        # session.commit()
--- seed.py
+++ seed.py
@@ -1,22 +1,46 @@
-import model
+from model import Rating, User, Movies, connect
+from datetime import datetime
+
 import csv
+
 
 def load_users(session):
     # use u.user
-    pass
+    with open('seed_data/u.user', 'rb') as csvfile:
+        f = csv.reader(csvfile, delimiter = "|")
+        for row in f:
+            user = User(age=row[1], zipcode=row[4])
+            session.add(user)
+        session.commit()
 
 def load_movies(session):
     # use u.item
-    pass
+    with open('seed_data/u.item', 'rb') as csvfile:
+        f = csv.reader(csvfile, delimiter= "|")
+        for row in f:
+            dateobject = row[2]
+            title = row[3]
+            title=title.decode("latin-1")
+            dateobject = datetime.strptime("01-Jan-1995", "%d-%b-%Y")
+            movie = Movies(name=title, released_at=dateobject, imdb_url=row[4])
+            session.add(movie)
+        session.commit()
 
 def load_ratings(session):
-    # use u.data
-    pass
+    with open('seed_data/u.data', 'rb') as csvfile:
+        f = csv.reader(csvfile, delimiter = "\t")
+        for row in f:
+            rating = Rating(movie_id=row[1], user_id=row[0], rating=row[2])
+            session.add(rating)
+        session.commit()
 
 def main(session):
     # You'll call each of the load_* functions with the session as an argument
-    pass
+    load_users(session)
+    load_movies(session)
+    load_ratings(session)
+    
 
 if __name__ == "__main__":
-    s= model.connect()
+    s= connect()
     main(s)
